5357ec739697fa9f9660710309e7df0d05c1a410
Nader Boutros <nadernabil2006@hotmail.com>
2023-09-19 15:52:45 -0400

    09-19-23 part One

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-const Note = () => (
+const Note = (props) => (
   <div className="note">
-    <h1>This is the note title</h1>
-    <p>This is the notr content</p>
+    <h1>{props.title}</h1>
+    <p>{props.content}</p>
   </div>
 );
 
